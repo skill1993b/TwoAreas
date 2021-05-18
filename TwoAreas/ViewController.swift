a5a938e12487e05c72ca5e0bd09ec03d985534a3
Nikita Zhuravlev <skill1993@icloud.com>
2021-05-18 22:18:30 +0300

    Initial Commit

---
+++ TwoAreas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoAreas
+//
+//  Created by Никита Журавлев on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
